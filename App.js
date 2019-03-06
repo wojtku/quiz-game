b3e6c287e5347b432f97c3bf1e769c1e724f2b5b
Wojciech Kaczmar <kaczmar.w@gmail.com>
2019-03-06 23:03:29 +0100

    refresh

--- App.js
+++ App.js
@@ -1,103 +1,142 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Audio } from 'expo';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Audio } from "expo";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.audioPlayer = new Audio.Sound();
+    this.resetState = this.resetState.bind(this);
     this.state = {
       button1Disabled: false,
       button2Disabled: false,
       button3Disabled: false,
       button1Color: {
-        backgroundColor: 'white'
+        backgroundColor: "white"
       },
       button2Color: {
-        backgroundColor: 'white'
+        backgroundColor: "white"
       },
       button3Color: {
-        backgroundColor: 'white'
+        backgroundColor: "white"
       }
-    }
+    };
   }
 
   playSound = async () => {
     try {
-      await this.audioPlayer.unloadAsync()
+      await this.audioPlayer.unloadAsync();
       await this.audioPlayer.loadAsync(require("./assets/buzz.mp3"));
       await this.audioPlayer.playAsync();
     } catch (err) {
-      console.warn("Couldn't Play audio", err)
+      console.warn("Couldn't Play audio", err);
     }
-}
+  };
+
+  resetState = () => {
+    this.setState({
+      button1Disabled: false,
+      button2Disabled: false,
+      button3Disabled: false,
+      button1Color: {
+        backgroundColor: "white"
+      },
+      button2Color: {
+        backgroundColor: "white"
+      },
+      button3Color: {
+        backgroundColor: "white"
+      }
+    })
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <View
-          pointerEvents={this.state.button1Disabled ? "none": undefined}
+          pointerEvents={this.state.button1Disabled ? "none" : undefined}
           style={[styles.buttonContainer, this.state.button1Color]}
-          onTouchStart={()=> {
-            this.playSound()
+          onTouchStart={() => {
+            this.playSound();
             this.setState({
               button1Color: {
-                backgroundColor: 'green'
+                backgroundColor: "green"
               },
               button2Color: {
-                backgroundColor: 'black'
+                backgroundColor: "black"
               },
               button3Color: {
-                backgroundColor: 'black'
+                backgroundColor: "black"
               },
               button2Disabled: true,
-              button3Disabled: true,
-            })
-          }}>
+              button3Disabled: true
+            });
+            setTimeout(
+              function() {
+                this.resetState();
+              }.bind(this),
+              3000
+            );
+          }}
+        >
           <Text style={styles.textContainer}>Drużyna 1</Text>
-      </View>
-      <View
-        pointerEvents={this.state.button2Disabled ? "none": undefined}
-        style={[styles.buttonContainer, this.state.button2Color]}
-        onTouchStart={()=> {
-          this.playSound()
-          this.setState({
-            button2Color: {
-              backgroundColor: 'green'
-            },
-            button1Color: {
-              backgroundColor: 'black'
-            },
-            button3Color: {
-              backgroundColor: 'black'
-            },
-            button3Disabled: true,
-            button1Disabled: true,
-          })
-        }}>
-        <Text style={styles.textContainer}>Drużyna 2</Text>
-      </View>
-      <View 
-        pointerEvents={this.state.button3Disabled ? "none": undefined}
-        style={[styles.buttonContainer, this.state.button3Color]}
-        onTouchStart={()=> {
-          this.playSound()
-          this.setState({
-            button3Color: {
-              backgroundColor: 'green'
-            },
-            button1Color: {
-              backgroundColor: 'black'
-            },
-            button2Color: {
-              backgroundColor: 'black'
-            },
-            button1Disabled: true,
-            button2Disabled: true,
-          })
-        }}>
-        <Text style={styles.textContainer}>Drużyna 3</Text>
-      </View>
+        </View>
+        <View
+          pointerEvents={this.state.button2Disabled ? "none" : undefined}
+          style={[styles.buttonContainer, this.state.button2Color]}
+          onTouchStart={() => {
+            this.playSound();
+            this.setState({
+              button2Color: {
+                backgroundColor: "green"
+              },
+              button1Color: {
+                backgroundColor: "black"
+              },
+              button3Color: {
+                backgroundColor: "black"
+              },
+              button3Disabled: true,
+              button1Disabled: true
+            });
+            setTimeout(
+              function() {
+                this.resetState();
+              }.bind(this),
+              3000
+            );
+          }}
+        >
+          <Text style={styles.textContainer}>Drużyna 2</Text>
+        </View>
+        <View
+          pointerEvents={this.state.button3Disabled ? "none" : undefined}
+          style={[styles.buttonContainer, this.state.button3Color]}
+          onTouchStart={() => {
+            this.playSound();
+            this.setState({
+              button3Color: {
+                backgroundColor: "green"
+              },
+              button1Color: {
+                backgroundColor: "black"
+              },
+              button2Color: {
+                backgroundColor: "black"
+              },
+              button1Disabled: true,
+              button2Disabled: true
+            });
+            setTimeout(
+              function() {
+                this.resetState();
+              }.bind(this),
+              3000
+            );
+          }}
+        >
+          <Text style={styles.textContainer}>Drużyna 3</Text>
+        </View>
       </View>
     );
   }
@@ -106,17 +145,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   buttonContainer: {
     flex: 1,
     borderWidth: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   textContainer: {
     fontSize: 40,
-    textAlign: 'center',
-    textAlignVertical: 'center' 
-  },
+    textAlign: "center",
+    textAlignVertical: "center"
+  }
 });
